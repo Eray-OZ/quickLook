b928cb102240bbb849e7ff740d63004b7a0a08e8
Eray <77747901+Eray-OZ@users.noreply.github.com>
2023-05-20 20:12:20 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,276 @@
+﻿using System;
+
+
+namespace console
+{
+
+
+
+    class Program
+    {
+
+
+
+
+        static void Main(string[] args)
+        {
+
+
+
+
+
+
+
+
+
+
+            // Implicit Conversion (Bilinçsiz)
+
+
+
+
+            // byte a = 5;
+
+            // sbyte b = 30;
+
+            // short c = 10;
+
+            // int d = a+b+c;       
+
+
+            // Console.WriteLine("d:"+d);
+
+
+            // long h = d;     
+
+            // float i = h;
+
+            // Console.WriteLine("i:"+i);
+
+
+
+            // string e = "zikriye";
+
+            // char f = 'k';
+
+            // object g = e +f+d;
+
+            // Console.WriteLine("g:"+g);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            // Explicit Conversion (Bilinçli)
+
+
+
+
+            // float w = 10.3f;
+
+            // int v = (int)w;
+
+            // Console.WriteLine("v:"+v);
+
+
+
+            // int xx = 6;
+
+            // string yy = xx.ToString();
+
+            // Console.WriteLine("yy:"+yy);
+
+
+            // string zz = 12.5f.ToString();
+
+            // Console.WriteLine("zz:"+zz);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            // Try Catch Finally
+
+
+            // try
+            // {
+            //     Console.WriteLine("Enter Number:");
+            //     int n1 = Convert.ToInt32(Console.ReadLine());
+            //     Console.WriteLine("Entered Number:"+n1);
+
+            // }
+            // catch(Exception ex)
+            // {
+            //     Console.WriteLine("Error: " + ex.Message.ToString());
+            // }
+            // finally
+            // {
+            //     Console.WriteLine("Done...");
+            // }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            // Ternary
+
+            // int time = DateTime.Now.Hour;
+
+            
+            // string res = time>=6 && time<11 ? "Morning" : time<=18 ? "Good Day" : "Nightyy";
+
+            // Console.WriteLine(res);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            // Switch Case
+
+
+            // int month = DateTime.Now.Month;
+
+
+
+            // switch (month)
+            // {
+            //     case 12:
+            //     case 1:
+            //     case 2:
+            //         Console.WriteLine("Winter");
+            //         break;
+
+
+            //     case 3:
+            //     case 4:
+            //     case 5:
+            //         Console.WriteLine("Spring");
+            //         break;
+
+
+            //     case 6:
+            //     case 7:
+            //     case 8:
+            //         Console.WriteLine("Summer");
+            //         break;
+
+
+            //     case 9:
+            //     case 10:
+            //     case 11:
+            //         Console.WriteLine("Fall");
+            //         break;
+
+
+            //     default:
+            //         break;                                                            
+            // }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            
+
+          
+        
+        
+        
+    
+           
+                
+
+
+
+
+
+
+        }
+    }
+}
